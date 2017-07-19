9853482c60ff12c409f951441d1dd780ddf76a8b
yaojingyan <791662115@qq.com>
2017-07-19 11:22:56 +0800

    姚静燕更新页面

---
+++ src/pages/login/login.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import {App, NavController, NavParams} from 'ionic-angular';
+import {MyPage} from "../my/my";
+import {TabsPage} from "../tabs/tabs";
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise'
+import {ZhucePage} from "../zhuce/zhuce";
+import {UserService} from "../../app/user.service";
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  user:{}={};
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public app:App,
+              public http:Http,
+              public us:UserService
+  ) {
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  gologin()
+  {
+    this.http.post('http://localhost:3000/loginx',this.user)
+      .toPromise()
+      .then((res)=>{
+      console.log(res);
+        if (res.json().success){
+          this.us.user = this.user;
+          this.app.getRootNav().setRoot(TabsPage);
+
+        }
+      })
+
+  }
+  godxlogin()
+  {
+
+  }
+
+  zhuce()
+  {
+    this.navCtrl.push(ZhucePage);
+  }
+
+
+
+
+}
